95309e32005636aeb2ee251a7ed0260b3964fdb5
sbleru <usareru@gmail.com>
2024-04-20 02:54:39 +0900

    chore: Add port

--- apps/api/src/endpoint/index.ts
+++ apps/api/src/endpoint/index.ts
@@ -13,7 +13,7 @@ export const startServer = async () => {
 
   app.post("/interactions", verifyDiscordRequest, interactionsHandler);
 
-  const port = 3000;
+  const port = Number(process.env.PORT) || 3000;
   console.log(`Server is running on port ${port}`);
 
   serve({
